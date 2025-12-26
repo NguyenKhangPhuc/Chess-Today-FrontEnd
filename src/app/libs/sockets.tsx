07265a3e3cadd34230fdb1735bd4155e06064b51
Khang Phuc Nguyen <nkpnkpnkp2005@gmail.com>
2025-12-26 11:02:37 +0200

    Feat: Adding middleware for route handling

--- src/app/libs/sockets.tsx
+++ src/app/libs/sockets.tsx
@@ -2,13 +2,12 @@ import { io, Socket } from "socket.io-client";
 let socket: Socket | null = null
 
 export const getSocket = () => {
-    console.log("Is socket null", socket)
+
     if (!socket) {
         socket = io('http://localhost:3001', {
             withCredentials: true,
         })
         socket.on("connect", () => console.log("Socket connected", socket!.id));
-        socket.on("connect_error", (err) => console.error("Connect error:", err));
     }
     return socket;
 }
---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+
+export function middleware(req: NextRequest) {
+    const token = req.cookies.get("access_token")
+    const { pathname } = req.nextUrl
+    console.log('[MIDDLEWARE]', req.nextUrl.pathname)
+    const guestOnly = ["/login", "/register"]
+    const privateRoutes = ['/game-management', '/profile', '/messages', '/friends', '/puzzles', '/history']
+
+    if (token && guestOnly.includes(pathname)) {
+        return NextResponse.redirect(new URL("/", req.url))
+    }
+
+    if (!token && privateRoutes.some(p => pathname.startsWith(p))) {
+        return NextResponse.redirect(new URL("/login", req.url))
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: ['/history/:path*', '/chess/:path*', '/game-management', '/profile', '/messages', '/friends', '/login', '/puzzles'],
+}
