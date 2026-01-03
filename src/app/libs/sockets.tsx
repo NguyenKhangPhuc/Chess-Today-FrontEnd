d66c979a0ec16094fac97b02f2780fe6b1c35eaa
Khang Phuc Nguyen <nkpnkpnkp2005@gmail.com>
2026-01-03 14:47:03 +0200

    Fix: Update socket url connection

--- src/app/libs/sockets.tsx
+++ src/app/libs/sockets.tsx
@@ -4,7 +4,7 @@ let socket: Socket | null = null
 export const getSocket = () => {
     if (!socket) {
         console.log("socket connection");
-        socket = io(process.env.NEXT_PUBLIC_API_URL ? process.env.NEXT_PUBLIC_API_URL : '', {
+        socket = io(process.env.NEXT_PUBLIC_SOCKET_URL ? process.env.NEXT_PUBLIC_SOCKET_URL : '', {
             withCredentials: true,
         })
         socket.on("connect", () => console.log("Socket connected", socket!.id));
